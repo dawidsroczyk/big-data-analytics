ac7c7bb4b86b95f5185f4105b0d676d5bdcb46cb
dsroczyk <dawid.sroczyk54@gmail.com>
2025-11-22 00:42:22 +0100

    working nifi-hdfs connection

--- hdfs/init-hdfs.sh
+++ hdfs/init-hdfs.sh
@@ -7,5 +7,6 @@ done
 hdfs dfs -mkdir -p /bronze/raw
 hdfs dfs -mkdir -p /silver/cleaned  
 hdfs dfs -mkdir -p /gold/business
-
+# Added permissive permissions for NiFi development writes
+hdfs dfs -chmod -R 777 /bronze /silver /gold
 echo "Medallion architecture created: /bronze /silver /gold"
