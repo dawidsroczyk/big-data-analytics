57497fb9ad6b88c705c878b422a3764c87a54086
OganKuba <kubog123@gmail.com>
2025-12-21 18:48:39 +0100

    spark + hive

--- preproccessing-module/src/main/scala/preprocessing/spark/SparkSessionBuilder.scala
+++ preproccessing-module/src/main/scala/preprocessing/spark/SparkSessionBuilder.scala
@@ -8,13 +8,14 @@ object SparkSessionBuilder {
       .appName(appName)
       .master("spark://spark-master:7077")   // IMPORTANT: use Docker master
       .config("spark.hadoop.fs.defaultFS", "hdfs://namenode:8020")
+      .enableHiveSupport()
       .getOrCreate()
   }
   def build_original(appName: String) : SparkSession = {
     SparkSession.builder()
       .appName(appName)
       .master(sys.env.getOrElse("SPARK_MASTER", "local[*]"))
-      // zakładam, że w klastrze Spark ma już skonfigurowany HDFS (core-site.xml, hdfs-site.xml)
+      .enableHiveSupport()
       .getOrCreate()
   }
 
