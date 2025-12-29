b69d52ebc99bd30abd1298aa3186d1d22c6d2f3e
dawidsroczyk <dawid.sroczyk54@gmail.com>
2025-12-29 18:20:00 +0000

    fix air quality spark

--- preproccessing-module/src/main/scala/preprocessing/job/AirQualityPreprocessingJob.scala
+++ preproccessing-module/src/main/scala/preprocessing/job/AirQualityPreprocessingJob.scala
@@ -1,11 +1,10 @@
 package preprocessing.job
 
+import org.apache.spark.sql.functions.col
 import preprocessing.config.PreprocessingConfig
 import preprocessing.spark.SparkSessionBuilder
 import preprocessing.io.RawReaders
 import preprocessing.transform.AirQualityPreprocessor
-
-import org.apache.spark.sql.functions._
 
 object AirQualityPreprocessingJob {
 
@@ -31,30 +30,6 @@ object AirQualityPreprocessingJob {
 
     println("[AirQualityPreprocessingJob] Done.")
 
-    spark.sql("CREATE DATABASE IF NOT EXISTS silver")
-
-    spark.sql(s"""
-      CREATE EXTERNAL TABLE IF NOT EXISTS silver.air_quality_clean (
-        lat DOUBLE,
-        lon DOUBLE,
-        event_ts TIMESTAMP,
-        event_hour INT,
-        pm25 DOUBLE,
-        pm10 DOUBLE,
-        no2 DOUBLE,
-        so2 DOUBLE,
-        o3 DOUBLE,
-        co DOUBLE,
-        nh3 DOUBLE,
-        aqi INT,
-        data_provider STRING
-      )
-      PARTITIONED BY (event_date STRING)
-      STORED AS PARQUET
-      LOCATION 'hdfs://namenode:8020${outputPath}'
-    """)
-
-    spark.sql("MSCK REPAIR TABLE silver.air_quality_clean")
     spark.stop()
   }
 }
